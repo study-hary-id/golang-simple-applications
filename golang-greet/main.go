c72f4bf9030578e4ccc223d5b19d73f8ab6d071f
study-hary-id <haryansyah.study.id@gmail.com>
2021-08-12 20:49:14 +0700

    Add simple error handling

--- golang-greet/main.go
+++ golang-greet/main.go
@@ -2,12 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
 	
 	"golang-greet/greeting"
 )
 
 func main() {
-	// Get a greeting message and print it.
-	message := greeting.Hello("Gladys")
+	// Set properties of the predefined logger, including the log entry prefix
+	// and a flag to disable printing the time, source file and line number.
+	log.SetPrefix("greeting: ")
+	log.SetFlags(0)
+
+	// Request a greeting message and expected an error.
+	message, error := greeting.Hello("")
+	if error != nil {
+		log.Fatal(error)
+	}
+	
 	fmt.Println(message)
 }
