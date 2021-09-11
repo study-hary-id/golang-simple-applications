abb24e9ecdf3702268801963f64bff8b4cd8173d
study-hary-id <haryansyah.study.id@gmail.com>
2021-09-11 15:15:23 +0700

    Add editHandler & refactor viewHandler

--- golang-web/handlers.go
+++ golang-web/handlers.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 // viewHandler handle http.Request to "/view/" route and
-// response the text/html file to the client.
+// render the wiki text/html file to the client.
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	var (
 		title = r.URL.Path[len("/view/"):]
@@ -14,21 +13,30 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	)
 
 	if title == "" {
-		// TODO: Create a function with name resBadRequest
-		w.WriteHeader(http.StatusBadRequest)
-		_, err := w.Write([]byte("400 status bad request"))
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
+		resBadRequest(w)
 		return
-
 	} else if err != nil {
 		http.NotFound(w, r)
 		return
 	}
 
-	_, err = fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", page.Title, page.Body)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+	renderTemplate(w, "view", page)
+}
+
+// editHandler handle http.Request to "/edit/" route and
+// used to edit the wiki from the client page site.
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	var (
+		title = r.URL.Path[len("/edit/"):]
+		page, err = loadPage(title)
+	)
+
+	if title == "" {
+		resBadRequest(w)
+		return
+	} else if err != nil {
+		page = &Page{Title: title}
 	}
+
+	renderTemplate(w, "edit", page)
 }
