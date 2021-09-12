1af12132133921157991d6c7a93517cb456ecd13
study-hary-id <haryansyah.study.id@gmail.com>
2021-09-12 13:39:19 +0700

    Refactor all handler in handlers.go

--- golang-web/handlers.go
+++ golang-web/handlers.go
@@ -4,56 +4,38 @@ import (
 	"net/http"
 )
 
-// viewHandler handle http.Request to "/view/" route and
+// viewHandler handles http.Request to "/view/" route and
 // render the wiki text/html file to the client.
-func viewHandler(w http.ResponseWriter, r *http.Request) {
-	title, err := getTitle(w, r)
-	if err != nil {
-		return
-	}
-
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
 	page, err := loadPage(title)
 	if err != nil {
-		http.Redirect(w, r, "/edit/" + title, http.StatusFound)
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
 		return
 	}
-
 	renderTemplate(w, "view", page)
 }
 
-// editHandler handle http.Request to "/edit/" route and
+// editHandler handles http.Request to "/edit/" route and
 // used to edit the wiki from the client page site.
-func editHandler(w http.ResponseWriter, r *http.Request) {
-	title, err := getTitle(w, r)
-	if err != nil {
-		return
-	}
-
+func editHandler(w http.ResponseWriter, _ *http.Request, title string) {
 	page, err := loadPage(title)
 	if err != nil {
 		page = &Page{Title: title}
 	}
-
 	renderTemplate(w, "edit", page)
 }
 
-// saveHandler handle http.Request to "/save/" route and
+// saveHandler handles http.Request to "/save/" route and
 // redirect to "/view/" after saving the wiki.
-func saveHandler(w http.ResponseWriter, r *http.Request) {
-	title, err := getTitle(w, r)
-	if err != nil {
-		return
-	}
-
+func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
 	var (
 		body = r.FormValue("body")
 		page = &Page{Title: title, Body: []byte(body)}
 	)
-	err = page.save()
+	err := page.save()
 	if err != nil {
 		resInternalServerError(w, err)
 		return
 	}
-
-	http.Redirect(w, r, "/view/" + title, http.StatusFound)
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
