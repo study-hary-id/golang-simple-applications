b9b78cfbf656d788faf2026ca9abda1e5f1b9899
study-hary-id <haryansyah.study.id@gmail.com>
2021-09-12 09:07:04 +0700

    Add templates global variable

--- golang-web/utils.go
+++ golang-web/utils.go
@@ -5,6 +5,11 @@ import (
 	"net/http"
 )
 
+// templates initialize available html templates.
+var templates = template.Must(template.ParseFiles(
+"view.html", "edit.html",
+))
+
 // resBadRequest serve response with Status Bad Request format.
 func resBadRequest(w http.ResponseWriter) {
 	w.WriteHeader(http.StatusBadRequest)
@@ -21,14 +26,9 @@ func resInternalServerError(w http.ResponseWriter, err error) {
 
 // renderTemplate used to render template page in html form.
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	t, err := template.ParseFiles(tmpl + ".html")
+	err := templates.ExecuteTemplate(w, tmpl + ".html", p)
 	if err != nil {
 		resInternalServerError(w, err)
 		return
-	}
-
-	err = t.Execute(w, p)
-	if err != nil {
-		resInternalServerError(w, err)
 	}
 }
