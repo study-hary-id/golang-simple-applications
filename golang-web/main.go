290ab338371708a4828085af9a02ff74db800695
study-hary-id <haryansyah.study.id@gmail.com>
2021-09-12 13:40:12 +0700

    Add docs and new handlers

--- golang-web/main.go
+++ golang-web/main.go
@@ -8,9 +8,14 @@ import (
 
 func main() {
 
-	http.HandleFunc("/view/", viewHandler)
-	http.HandleFunc("/edit/", editHandler)
-	//http.HandleFunc("/save/", saveHandler)
+	// GET /view/title:string
+	http.HandleFunc("/view/", makeHandler(viewHandler))
+
+	// GET /edit/title:string
+	http.HandleFunc("/edit/", makeHandler(editHandler))
+
+	// POST /save/title:string
+	http.HandleFunc("/save/", makeHandler(saveHandler))
 
 	fmt.Println("Server listening at http://localhost:8000/")
 	log.Fatal(http.ListenAndServe(":8000", nil))
