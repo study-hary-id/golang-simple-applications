fd4241b9940c9ea7a7d08ad9eb58c88db08cf9fd
study-hary-id <haryansyah.study.id@gmail.com>
2021-09-12 08:17:45 +0700

    Add resInternalServerError & implement

--- golang-web/utils.go
+++ golang-web/utils.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-// resBadRequest serve response with Bad Request format.
+// resBadRequest serve response with Status Bad Request format.
 func resBadRequest(w http.ResponseWriter) {
 	w.WriteHeader(http.StatusBadRequest)
 	_, err := w.Write([]byte("400 status bad request"))
@@ -14,11 +14,21 @@ func resBadRequest(w http.ResponseWriter) {
 	}
 }
 
+//resInternalServerError serve response with Status Internal Server Error format.
+func resInternalServerError(w http.ResponseWriter, err error) {
+	http.Error(w, err.Error(), http.StatusInternalServerError)
+}
+
 // renderTemplate used to render template page in html form.
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	t, _ := template.ParseFiles(tmpl + ".html")
-	err := t.Execute(w, p)
+	t, err := template.ParseFiles(tmpl + ".html")
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		resInternalServerError(w, err)
+		return
+	}
+
+	err = t.Execute(w, p)
+	if err != nil {
+		resInternalServerError(w, err)
 	}
 }
