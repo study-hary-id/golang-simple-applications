298a12630abf1f3900d5078650404cf5e995317c
study-hary-id <haryansyah.study.id@gmail.com>
2021-09-12 13:29:42 +0700

    Replace .ParseFiles into .ParseGlob

--- golang-web/utils.go
+++ golang-web/utils.go
@@ -1,19 +1,12 @@
 package main
 
 import (
-	"errors"
 	"html/template"
 	"net/http"
-	"regexp"
 )
 
-// templates initialize available html templates.
-var templates = template.Must(template.ParseFiles(
-"view.html", "edit.html",
-))
-
-// validPath initializes regexp rule to get a valid URL.
-var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+// templates initialize available html template in tmpl/ directory.
+var templates = template.Must(template.ParseGlob("tmpl/*"))
 
 // resBadRequest serves response with Status Bad Request format.
 func resBadRequest(w http.ResponseWriter) {
@@ -31,19 +24,9 @@ func resInternalServerError(w http.ResponseWriter, err error) {
 
 // renderTemplate used to render template page in html form.
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	err := templates.ExecuteTemplate(w, tmpl + ".html", p)
+	err := templates.ExecuteTemplate(w, tmpl+".html", p)
 	if err != nil {
 		resInternalServerError(w, err)
 		return
 	}
-}
-
-// getTitle returns the requested wiki title and error if invalid.
-func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
-	path := validPath.FindStringSubmatch(r.URL.Path)
-	if path == nil {
-		http.NotFound(w, r)
-		return "", errors.New("invalid page title")
-	}
-	return path[2], nil
 }
